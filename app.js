0dd1491529809e2682e49dcd32b69cd35ded6a46
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-01-20 15:36:06 +0100

    feat: add posts

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("hola")
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.json(`Questo è il server del mio blog, vai a visualizzare i post http://localhost:${port}/bacheca`)
+})
+
+app.get('/bacheca', (req, res) => {
+    res.json([
+        {
+            title :'ciao a tutti',
+            bodyPost : 'lorem ipsum'
+        },
+        {
+            title :'ciao a tutti',
+            bodyPost : 'lorem ipsum'
+        },
+        {
+            title :'ciao a tutti',
+            bodyPost : 'lorem ipsum'
+        },
+        {
+            title :'ciao a tutti',
+            bodyPost : 'lorem ipsum'
+        },
+        {
+            title :'ciao a tutti',
+            bodyPost : 'lorem ipsum'
+        }
+    ])
+})
+ 
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
