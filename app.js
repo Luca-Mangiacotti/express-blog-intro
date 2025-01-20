9eda755a4ad8088fe1424f1e3bae7e662e391ab8
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-01-20 16:02:15 +0100

    feat: add images as static files

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.json(`Questo è il server del mio blog, vai a visualizzare i post http://localhost:${port}/bacheca`)
 })
@@ -10,24 +12,26 @@ app.get('/bacheca', (req, res) => {
     res.json([
         {
             title :'ciao a tutti',
+            image: `http://localhost:${port}/images/ciambellone.jpeg`,
             bodyPost : 'lorem ipsum'
         },
         {
             title :'ciao a tutti',
+            image: `http://localhost:${port}/images/cracker_barbabietola.jpegp`,
+            bodyPost : 'lorem ipsum'
+        }, {
+            title :'ciao a tutti',
+            image: `http://localhost:${port}/images/pane_fritto_dolce.jpeg`,
+            bodyPost : 'lorem ipsum'
+        }, {
+            title :'ciao a tutti',
+            image: `http://localhost:${port}/images/pasta_barbabietola.jpeg`,
+            bodyPost : 'lorem ipsum'
+        }, {
+            title :'ciao a tutti',
+            image: `http://localhost:${port}/images/torta_paesana.jpeg`,
             bodyPost : 'lorem ipsum'
         },
-        {
-            title :'ciao a tutti',
-            bodyPost : 'lorem ipsum'
-        },
-        {
-            title :'ciao a tutti',
-            bodyPost : 'lorem ipsum'
-        },
-        {
-            title :'ciao a tutti',
-            bodyPost : 'lorem ipsum'
-        }
     ])
 })
  
